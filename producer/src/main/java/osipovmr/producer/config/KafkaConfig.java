8015e200f5923bb4dbcb777cd7b21ccf1fff84cd
Осипов Илья Александрович <osipovmr@yandex.ru>
2024-06-28 12:56:38 +0300

    V1

--- producer/src/main/java/osipovmr/producer/config/KafkaConfig.java
+++ producer/src/main/java/osipovmr/producer/config/KafkaConfig.java
@@ -3,8 +3,10 @@ package osipovmr.producer.config;
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
@@ -12,7 +14,6 @@ import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaAdmin;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
-import osipovmr.producer.config.yaml.KafkaProducerYamlProperties;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -21,14 +22,15 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class KafkaConfig {
 
-    private final KafkaProducerYamlProperties producerYamlProperties;
+    @Value(value = "${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
 
     @Bean
     public ProducerFactory<String, String> producerFactory() {
         Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.putAll(producerYamlProperties.getPropertiesMap());
         return new DefaultKafkaProducerFactory<>(props);
     }
 
@@ -45,7 +47,7 @@ public class KafkaConfig {
     @Bean
     KafkaAdmin admin() {
         Map<String, Object> configs = new HashMap<>();
-        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, producerYamlProperties.getPropertiesMap().get("bootstrap.servers"));
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         return new KafkaAdmin(configs);
     }
 }
--- producer/src/main/java/osipovmr/producer/controller/Controller.java
+++ producer/src/main/java/osipovmr/producer/controller/Controller.java
@@ -15,10 +15,12 @@ public class Controller {
 
     private final KafkaTemplate<String, String> kafkaTemplate;
 
+    private int number;
+
     @GetMapping("/start")
     public ResponseEntity<Void> start() {
         for (int i = 0; i < 1000; i++) {
-            kafkaTemplate.send("osipov", UUID.randomUUID().toString());
+            kafkaTemplate.send("osipov", UUID.randomUUID().toString(), "Message number: " + number++);
         }
         return new ResponseEntity<>(HttpStatus.OK);
     }
